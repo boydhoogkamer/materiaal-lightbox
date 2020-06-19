924dc71ba4318473f1826ca715ebc09aa5263d20
boydhoogkamer <43574994+boydhoogkamer@users.noreply.github.com>
2020-06-19 12:11:10 +0200

    Video 6

--- js/lightbox.js
+++ js/lightbox.js
@@ -3,7 +3,17 @@ const allCards = document.querySelectorAll('.card');
 
 let cardContent = [];
 
-for(let i=0; i< allContent.length; i++) {
+for(let i=0; i < allContent.length; i++) {
     cardContent.push(allContent[i]);
     allContent[i].remove();
+}
+
+function CreateModal(num) {
+
+}
+
+for(let i=0; i < allCards.length; i++) {
+    allCards[i].addEventListener('click', function(){
+        CreateModal(i)
+    })
 }
