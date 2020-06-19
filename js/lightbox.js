236f007bf1617606a62d751e933575300fe80dcb
boydhoogkamer <43574994+boydhoogkamer@users.noreply.github.com>
2020-06-19 12:19:18 +0200

    Video 7 + 8

--- js/lightbox.js
+++ js/lightbox.js
@@ -9,7 +9,15 @@ for(let i=0; i < allContent.length; i++) {
 }
 
 function CreateModal(num) {
+    let modal = document.createElement('div');
+    modal.className = 'modal';
 
+
+    let modalContainer = document.createElement('div');
+    modalContainer.className = 'modal-container';
+    modalContainer.innerHTML = cardContent[num].innerHTML;
+    modal.append(modalContainer);
+    document.body.append(modal);
 }
 
 for(let i=0; i < allCards.length; i++) {
